72d69ef7d8e56c4f86f8f8b5e22a206b53a8a31b
Achyut Kumar <achuetch2008@gmail.com>
2026-03-12 10:51:05 +0530

    initial commit

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,20 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://soulslates.com";
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      // Disallow any private routes if you add them later, e.g., disallow: '/dashboard/'
-    },
-    sitemap: 'https://soulslates.com/sitemap.xml', // Replace with your actual domain
-  }
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
 }
